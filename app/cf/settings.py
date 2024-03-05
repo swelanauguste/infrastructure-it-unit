1b6c5d7269c27535c5b2899831f699738441d933
swelanauguste <swelanauguste@gmail.com>
2024-03-05 12:55:31 -0400

    email

--- app/cf/settings.py
+++ app/cf/settings.py
@@ -157,7 +157,7 @@ EMAIL_HOST_USER = os.environ.get("EMAIL")
 EMAIL_HOST_PASSWORD = os.environ.get("PASS")
 EMAIL_PORT = 587
 EMAIL_USE_TLS = False
-EMAIL_USE_SSL = False
+EMAIL_USE_SSL = True
 
 SITE_ID = 1
 
